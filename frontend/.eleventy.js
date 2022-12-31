47bade2f84156942b5efbfb0d4ceffe6d4f16153
juju <hyceate+github@gmail.com>
2022-12-31 10:06:46 -0500

    1

--- frontend/.eleventy.js
+++ frontend/.eleventy.js
@@ -1,5 +1,18 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 module.exports = function(eleventyConfig) {
-	eleventyConfig.addPlugin(pluginWebc);
+	eleventyConfig.addPlugin(pluginWebc, {
+		// Glob to find no-import global components
+		// (The default changed from `false` in Eleventy WebC v0.7.0)
+		components: "_components/**/*.webc",
+		// Adds an Eleventy WebC transform to process all HTML output
+		useTransform: false,
+		// Additional global data used in the Eleventy WebC transform
+		transformData: {},
+	});
+    // Let Eleventy transform HTML files as webc
+  // So that we can use .html instead of .webc
+  return {
+    htmlTemplateEngine: "webc"
+  };
 };
