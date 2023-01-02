6521e1a0a01f594a23482cc83273e9da14693baa
juju <hyceate+github@gmail.com>
2023-01-02 07:20:42 -0500

    update

--- frontend/.eleventy.js
+++ frontend/.eleventy.js
@@ -1,18 +1,32 @@
+const yaml = require("js-yaml");
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("admin");
+	eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+	eleventyConfig.addPlugin(EleventyRenderPlugin);
 	eleventyConfig.addPlugin(pluginWebc, {
 		// Glob to find no-import global components
 		// (The default changed from `false` in Eleventy WebC v0.7.0)
-		components: "_components/**/*.webc",
+		components: "src/_components/**/*.webc",
 		// Adds an Eleventy WebC transform to process all HTML output
 		useTransform: false,
 		// Additional global data used in the Eleventy WebC transform
 		transformData: {},
 	});
-    // Let Eleventy transform HTML files as webc
-  // So that we can use .html instead of .webc
+	eleventyConfig.addNunjucksShortcode("youtube", function (youtubeId, aspectRatio) {
+		return `<div class="aspect-ratio" style="--aspect-ratio: ${aspectRatio}"><iframe class="youtube-player video video--youtube" src="https://www.youtube.com/embed/${youtubeId}/" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`;
+	  });
+    // Let Eleventy transform HTML files 
+  // So that we can use .html
   return {
-    htmlTemplateEngine: "webc"
+	dir: {
+		input: "src"
+	},
+	markdownTemplateEngine: "njk",
+	dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
   };
 };
